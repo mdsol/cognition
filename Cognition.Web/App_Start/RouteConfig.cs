1a6a4d67b2613b7f4c2effb6d920223b63145c7d
Ed Andersen <eandersen@mdsol.com>
2013-07-03 16:41:04 +0900

    Lowercase routes

--- Cognition.Web/App_Start/RouteConfig.cs
+++ Cognition.Web/App_Start/RouteConfig.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using Cognition.Web.Helpers;
 
 namespace Cognition.Web
 {
@@ -13,7 +14,7 @@ namespace Cognition.Web
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
+            routes.MapRouteLowercase(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
---
+++ Cognition.Web/Controllers/DocumentController.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cognition.Web.Controllers
+{
+    public class DocumentController : Controller
+    {
+        //
+        // GET: /Document/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
